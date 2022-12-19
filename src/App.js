8a06cdebd4a0d7d2e2bc7107d9ea9779b4798b63
junaidapdev <junaidap.dev@gmail.com>
2022-12-19 10:26:59 +0530

    Add feedback

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import {v4 as uuidv4} from 'uuid'
 import { useState } from 'react'
  
 
@@ -12,6 +13,11 @@ import { useState } from 'react'
 const App = () => {
 
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback,...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure?')) {
@@ -24,7 +30,7 @@ const App = () => {
   <>
     <Header/>
     <div className="container">
-      <FeedbackForm/>
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import { motion, AnimePresence } from 'framer-motion'
+
 
 const FeedbackList = ({ feedback, handleDelete }) => {
 
-   if(!feedback || feedback.length === 0){
-        return<p>No Feedback</p>
-   }
-    
-  return (
-    <div className='feedback-list'>
-        {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} 
-            handleDelete={handleDelete}
-            />
-        ))}
-    </div>
-  )
+    if (!feedback || feedback.length === 0) {
+        return <p>No Feedback</p>
+    }
+
+
+    // return (
+    //     <div className='feedback-list'>
+    //         <AnimePresence>
+    //             {feedback.map((item) => (
+    //                 <motion.div>
+    //                     <FeedbackItem key={item.id} item={item}
+    //                         handleDelete={handleDelete} />
+    //                 </motion.div>
+    //             ))}
+    //         </AnimePresence>
+    //     </div>
+    // )
+
+
+      return (
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <FeedbackItem key={item.id} item={item} 
+                handleDelete={handleDelete}
+                />
+            ))}
+        </div>
+      )
 }
 
 FeedbackList.propTypes = {
