0a5610f8470a2bbbaec1f537ab8eb6473c10eb2e
junaidapdev <junaidap.dev@gmail.com>
2022-12-19 09:52:44 +0530

    Feedback form  added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,26 +1,50 @@
 import React, { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 
 const FeedbackForm = () => {
 
     const [text, setText] = useState('')
+    const [rating, setrating] = useState(10)
+    const [btnDisdabled, setBtnDisdabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+
+        if(text === '') {
+            setBtnDisdabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be atleast 10 characters')
+            setBtnDisdabled(true)
+        } else {
+            setBtnDisdabled(false)
+            setMessage(null)
+        }
+
+
         setText(e.target.value)
     }
 
-  return (
-    <Card>
-       <h2>How would you rate your service with us?</h2> 
+    return (
+        <Card>
+            <form>
+                <h2>How would you rate your service with us?</h2>
 
-       <div className="input-group">
-    <input onChange={handleTextChange} value={text} type="text" placeholder="Write a Review" />
-        <Button type='submit'>Send</Button>
-       </div>
-    </Card>
-  )
+                <RatingSelect/>
+
+                <div className="input-group">
+                    <input onChange={handleTextChange} value={text} type="text" placeholder="Write a Review" />
+                    <Button type='submit' isDisabled={btnDisdabled}>Send</Button>
+                </div>
+
+                {message && <div className='message'>{message}</div>}
+
+            </form>
+        </Card>
+    )
 }
 
 export default FeedbackForm
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const RatingSelect = () => {
+
+    const [selected, setSelected] = useState(10)
+
+
+  return (
+    <div>RatingSelect</div>
+  )
+}
+
+export default RatingSelect
